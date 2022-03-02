d1efd15d76fdfc4b0c037bcb9befc214f45f4a8b
Vagner Filho <vagner.filhopro@gmail.com>
2022-03-02 10:24:51 -0400

    setup completed

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,15 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<style>
+<style lang="scss">
 #app {
+  $test-color: blue;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: $test-color;
 }
 </style>
