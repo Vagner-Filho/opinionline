e45fc03a3bddc92c1eb548dd1fa75ed23709786b
Vagner Filho <vagner.martins@sesims.com.br>
2022-03-30 12:35:55 -0400

    basic testing

--- cypress/integration/playground/first.spec.js
+++ cypress/integration/playground/first.spec.js
@@ -1,5 +1,12 @@
+/// <reference types="Cypress" />
 describe('My First Test', () => {
-  it('Should be true', () => {
-    expect(true).to.equal(true)
+  it('Visitis the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('type').click()
+    cy.pause()
+    cy.url().should('include', '/commands/actions')
+    cy.get('.action-email')
+    .type('fake@email.com')
+    .should('have.value', 'fake@email.com')
   })
 })
