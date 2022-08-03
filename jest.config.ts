2cccad8b3b5701661fa8e5a2ef0794fa68780312
Vagner Filho <vagner.filhopro@gmail.com>
2022-08-03 18:58:49 -0400

    fix: ignore coverage

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,6 @@ const config: Config.InitialOptions = {
   testEnvironment: "jsdom",
   extensionsToTreatAsEsm: [".ts"],
   moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node', 'd.ts'],
-  rootDir: './test/components',
   transform: {
     "\\.[jt]s?$": "ts-jest",
     "^.+\\.vue$": "@vue/vue3-jest" // Update to match your installed version
