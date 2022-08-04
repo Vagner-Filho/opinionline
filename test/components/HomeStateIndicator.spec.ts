9ef97edd24b41a39073ab314e9410ff43cdec684
Vagner Filho <vagner.filhopro@gmail.com>
2022-08-04 16:55:32 -0400

    chore: HomeStateIndicator

--- test/components/HomeStateIndicator.spec.ts
+++ test/components/HomeStateIndicator.spec.ts
@@ -1,6 +1,26 @@
+import HomeStateIndicator from '../../components/reader/HomeStateIndicator.vue';
+import { shallowMount } from '@vue/test-utils'; 
+
 describe('Home Indicator', () => {
-  test('go to most read', () => {})
-  test('go to most recent', () => {})
-  test('changes text when in most read', () => {})
-  test('changes text when in most recent', () => {})
+  const wrapper = shallowMount(HomeStateIndicator);
+
+  test('emits click event', async () => {
+    await wrapper.find('span').trigger('click');  
+
+    expect(wrapper.emitted().click).toBeTruthy();
+  })
+  test('emits click event with api route', async () => {
+    await wrapper.find('span').trigger('click');
+
+    expect(wrapper.emitted().click[0][0]).toEqual('mostRead');
+    expect(wrapper.emitted().click[1][0]).toEqual('mostRecent');
+  })
+  test('text when in most recent', () => {
+    expect(wrapper.find('span').text()).toEqual('Ver mais lidos')
+  })
+  test('text when in most read', async () => {
+    await wrapper.find('span').trigger('click');  
+
+    expect(wrapper.find('span').text()).toEqual('Ver mais recentes')
+  })
 })
