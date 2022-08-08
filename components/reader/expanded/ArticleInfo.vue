fb304ca10b3cd51cd4e84372e863c64d7555154e
Vagner Filho <vagner.martins@sesims.com.br>
2022-08-08 15:09:25 -0400

    chore: expanded article components

--- components/reader/expanded/ArticleInfo.vue
+++ components/reader/expanded/ArticleInfo.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-
+  <div class="w-full">
+    <section class="w-1/2">
+      <main class="flex">
+        <h1>
+          {{ title }}
+        </h1>
+        <div id="article-tags" class="flex">
+          <ArticleTag v-for="tag in tags" :tag-name="tag"/>
+        </div>
+      </main>
+    </section>
+    <div class="w-1/2">
+      <img v-if="authorPic" :src="authorPic" :alt="'autor(a) de: ' + title">
+      <img v-else :src="defaulAuthorPic" :alt="'autor(a) de: ' + title">
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import ArticleTag from './ArticleTag.vue';
+  defineProps<{
+    title: string
+    tags: string[]
+    releaseDate: string
+    authorPic: string
+    defaulAuthorPic: string
+  }>();
 </script>
 
 <style scoped>
---
+++ components/reader/expanded/ArticleTag.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="category-tag">
+    {{ tagName }}
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    tagName: string
+  }>();
+</script>
