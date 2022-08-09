45b903e3461b673f114192430c281ca23363fe3a
Vagner Filho <vagner.martins@sesims.com.br>
2022-08-09 09:37:10 -0400

    chore: ArticleCover test

--- components/reader/expanded/ArticleCover.vue
+++ components/reader/expanded/ArticleCover.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-
+  <div id="article-cover">
+    <img v-if="coverPath" :src="coverPath" :alt="'capa do artigo: ' + articleTitle">
+    <img v-else :src="defaultCoverPath" :alt="'capa do artigo: ' + articleTitle">
   </div>
 </template>
 
 <script setup lang="ts">
-
+  defineProps<{
+    coverPath: string | null;
+    defaultCoverPath: string;
+    articleTitle: string;
+  }>()
 </script>
-
-<style scoped>
-
-</style>
