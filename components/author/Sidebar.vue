8243ae0ace2e98de0a04500a50e58c635aa30e43
Vagner Filho <vagner.martins@sesims.com.br>
2022-08-15 15:03:34 -0400

    chore: sidebar component

---
+++ components/author/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="sidebar">
+    <nav>
+      <ul>
+        <li class="profile" @click="$router.push({ name: 'author-profile'})">
+          <img src="/public/author/profile.svg" alt="perfil">
+        </li>
+        <li class="home" @click="$router.push({ name: 'author-home'})">
+          <img src="/public/author/home.svg" alt="home">
+        </li>
+        <li class="create" @click="$router.push({ name: 'author-article', query: { isNew: '1' }})">
+          <img src="/public/author/plus.svg" alt="criar artigo">
+        </li>
+        <li class="articles" @click="$router.push({ name: 'author-article', query: { isNew: '0' }})">
+          <img src="/public/author/open-book.svg" alt="ver artigos">
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- test/components/author/SideBar.spec.ts
+++ test/components/author/SideBar.spec.ts
@@ -1,6 +1,39 @@
+import Sidebar from '../../../components/author/Sidebar.vue';
+import { shallowMount } from '@vue/test-utils';
+
+const push = jest.fn((routerObj) => {
+  return routerObj;
+})
+
 describe('SideBar', () => {
-  test('renders nav options', () => {})
-  test('opens', () => {})
-  test('closes', () => {})
-  test('redirects to correct page', () => {})
+  const wrapper = shallowMount(Sidebar, {
+    global: {
+      mocks: {
+        $router: {
+          push
+        }
+      }
+    }
+  });
+  test('renders nav options', () => {
+    const navOptions = wrapper.findAll('img');
+    const optionsPublicPath = ['/public/author/profile.svg', '/public/author/home.svg', '/public/author/plus.svg', '/public/author/open-book.svg']
+
+    for (const opt in navOptions) {
+      expect(navOptions[opt].attributes('src')).toBe(optionsPublicPath[opt])
+    }
+  })
+  test('redirects to correct page', async() => {
+    const classes = ['.profile', '.home', '.create', '.articles'];
+    const expectedRouterObj = [
+      { name: 'author-profile' },
+      { name: 'author-home' },
+      { name: 'author-article', query: { isNew: '1' } },
+      {name: 'author-article', query: { isNew: '0' } }
+    ]
+    for (const rtr in expectedRouterObj) {
+      await wrapper.find(classes[rtr]).trigger('click');
+      expect(push).toHaveBeenCalledWith(expectedRouterObj[rtr]);
+    }
+  })
 })
