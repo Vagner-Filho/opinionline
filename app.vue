01c56ddb19a4faac3c5318697281588843d5b2e8
Vagner Filho <vagner.martins@sesims.com.br>
2022-08-18 12:12:52 -0400

    app title

--- app.vue
+++ app.vue
@@ -1,26 +1,12 @@
 <template>
   <div>
+    <Title>Opinionline</Title>
     <NuxtPage />
   </div>
 </template>
 
 <script setup lang="ts">
-// import { initializeApp } from 'firebase/app';
-// import { provide } from 'vue';
 import './assets/css/tailwind.css';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDsDGNI_nsTLccoc0qVYV3cGfv1TtJ6-KU",
-//   authDomain: "opinionline-77a42.firebaseapp.com",
-//   projectId: "opinionline-77a42",
-//   storageBucket: "opinionline-77a42.appspot.com",
-//   messagingSenderId: "370890248236",
-//   appId: "1:370890248236:web:03c97fb2c02a5fbb99c189",
-//   measurementId: "G-H7Z9WTPC0Q"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// provide(firebaseAppKey(), app);
 </script>
 
 <style>
