6fb41f4ef17255eedb748cc19427267016b89cfe
Vagner Filho <vagner.martins@sesims.com.br>
2022-08-24 10:58:52 -0400

    chore: tag selector

--- components/author/article/Form.vue
+++ components/author/article/Form.vue
@@ -1,6 +1,12 @@
 <template>
   <form class="max-w-4xl flex flex-col justify-center m-auto px-2 h-full max-h-[800px]" @submit.prevent="$emit('article', articleData)">
     <input type="text" class="std-author-input w-full h-10 mt-[35px]" v-model="articleData.title" :placeholder="titlePlaceholder" :disabled="isViewOnly" required>
+    <SelectFilter
+      :items-filter="tags"
+      :filter-id="'tag-selector'" 
+      :filter-name="'+ Tag'"
+      class="max-w-[80px] mt-3"
+    /> 
     <label for="cover-input" class="std-author-input w-full h-14 leading-[3.5rem] text-[#9fa6b2] text-center mt-[90px] mb-1">{{ coverPlaceholder }}</label>
     <input ref="coverInput" type="file" id="cover-input" class="hidden" disabled>
     <textarea name="text" id="article-text" class="std-author-input py-4" v-model="articleData.text" cols="30" rows="10" :placeholder="articleTextPlaceholder" :disabled="isViewOnly" required></textarea>
@@ -42,6 +48,12 @@
     cover: false, // null might break firebase db
     text: ''
   })
+  const tags = ref([
+    {
+      itemId: 0,
+      itemName: 'Criar tag'
+    }
+  ])
 
 
   watchEffect(() => {
